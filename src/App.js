33948edc90bedf66300b4cdb07020bab35a669be
tarushm2006 <tarushmohindru1515@gmail.com>
2021-01-26 17:31:53 +0530

    2 files modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import Header from "./Components/header.jsx";
 import Create from "./Components/create.jsx";
-import Display from "./Components/display.jsx";
-
+import "./Components/styles.css";
 export default function App() {
-  const [list,setList] = useState([]);
-  function add(data){
-  setList([...list,data]);
+  const [list, setList] = useState([]);
+  function add(data) {
+    setList(list => [...list, data]);
+  }
+  const listStyle = {
+    background: "dodgerBlue",
+    padding: "10px",
+    borderRadius: "5px",
+    color:"white",
+  };
+  function remove(e){
+    const index = e;
+   setList(list.filter(item=>item!== index))
   }
   return (
     <div>
       <Header />
       <Create add={add} />
-      <Display data={list}/>
+      <ul>
+        {list.map((val, ind) => (
+          <li key={ind} onClick={()=>{
+            remove(val);
+          }}>
+           <span style={listStyle}>{val}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
